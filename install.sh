22daed0b45de2c43c54ec03f89b2d68feea7caf9
yellhtetaung <yairhtet248.yha@gmail.com>
2022-10-09 02:19:39 +0630

    updated

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 sudo apt install -y curl git vlc google-chrome-stable gcc python2 python-is-python3 python3-pip telegram-desktop gimp gnome-tweaks gnome-shell-extensions vim htop neofetch
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:gerardpuig/ppa
@@ -7,15 +7,16 @@ sudo apt install ubuntu-cleaner
 sudo add-apt-repository ppa:obsproject/obs-studio
 sudo apt update
 sudo apt install ffmpeg obs-studio
-wget https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb -P ~/Downloads/
-wget https://az764295.vo.msecnd.net/stable/64bbfbf67ada9953918d72e1df2f4d8e537d340e/code_1.72.0-1664926972_amd64.deb -P ~/Downloads/
-wget https://dl.discordapp.net/apps/linux/0.0.20/discord-0.0.20.deb -P ~/Downloads/
-wget https://zoom.us/client/5.12.0.4682/zoom_amd64.deb -P ~/Downloads/
-wget https://github.com/agalwood/Motrix/releases/download/v1.6.11/Motrix_1.6.11_amd64.deb -P ~/Downloads/
-cd ~/Downloads/
+mkdir ~/Downloads/Software
+wget https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb -P ~/Downloads/Software
+wget https://az764295.vo.msecnd.net/stable/64bbfbf67ada9953918d72e1df2f4d8e537d340e/code_1.72.0-1664926972_amd64.deb -P ~/Downloads/Software
+wget https://dl.discordapp.net/apps/linux/0.0.20/discord-0.0.20.deb -P ~/Downloads/Software
+wget https://zoom.us/client/5.12.0.4682/zoom_amd64.deb -P ~/Downloads/Software
+wget https://github.com/agalwood/Motrix/releases/download/v1.6.11/Motrix_1.6.11_amd64.deb -P ~/Downloads/Software
+cd ~/Downloads/Software
 sudo dpkg -i *.deb
-sudo apt install -f
-sudo apt update && sudo apt upgrade
+sudo apt install -f -y
+sudo apt update && sudo apt upgrade -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 vim ~/.bashrc
 nvm install node
